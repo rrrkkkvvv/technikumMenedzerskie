3e22ea57de10cff8a167ef9cff2132764a43f00f
Roman <136304611+rrrkkkvvv@users.noreply.github.com>
2024-10-18 21:44:58 +0200

    Installed and initializated react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Logo from "./ui/Logo";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { urls } from "./constants/stringConstants";
+ 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Logo />,
+  },
+], {basename: urls.basename});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <Logo /> */}
+    <RouterProvider router={router} />
+
   </StrictMode>
 );
